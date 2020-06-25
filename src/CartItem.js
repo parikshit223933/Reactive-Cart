dff903d538ee5364ea46e10709f83900e1b17e41
Parikshit Singh <pk223933@gmail.com>
2020-06-25 09:37:37 +0530

    handling state of cart item, reason of using arrow functions

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,27 +2,46 @@ import React from 'react';
 
 class CartItem extends React.Component
 {
+    constructor()
+    {
+        super();
+        this.state =
+        {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+        /* this.increaseQuantity=this.increaseQuantity.bind(this); */
+    }
+    increaseQuantity = () =>/* arrow functions bind the balue of "this" of the function to the respective class. */
+    {
+        console.log(this);
+        return;
+    }
     render()
     {
+        const { price, title, qty } = this.state
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>
-                        Item
+                    <div style={{ fontSize: 25 }}>
+                        {title}
                     </div>
-                    <div style={{color:'grey'}}>
-                        Price
+                    <div style={{ color: 'grey' }}>
+                        Rs. {price}
                     </div>
-                    <div style={{color:'grey'}}>
-                        Quantity
+                    <div style={{ color: 'grey' }}>
+                        {qty} Unit
                     </div>
                     <div className="cart-item-actions">
-                        {/* all the cart items */}
-
-                    </div> 
+                        <div onClick={this.increaseQuantity}><i className="action-icons fas fa-trash-alt" ></i></div>
+                        <div onClick={this.increaseQuantity}><i className="action-icons fas fa-plus-square" ></i></div>
+                        <div onClick={this.increaseQuantity}><i className="action-icons fas fa-minus-square"></i></div>
+                    </div>
                 </div>
             </div>
         );
@@ -30,14 +49,14 @@ class CartItem extends React.Component
 };
 /* our class CartItem will inherit features from the class React.Component. extends indicates
 inheritance in javascript */
-const styles=
+const styles =
 {
     image:
     {
-        height:100,
-        width:110,
-        borderRadius:4,
-        backgroundColor:'orangered'
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        backgroundColor: 'orangered'
     }
 }
 export default CartItem;
