8ae9318c8b1b48fd97b8172b91ce2cc4de91896e
Parikshit Singh <pk223933@gmail.com>
2020-06-25 11:35:49 +0530

    set state in depth studied, studied callback function in set state method

--- src/CartItem.js
+++ src/CartItem.js
@@ -17,24 +17,40 @@ class CartItem extends React.Component
     increaseQuantity = () =>/* arrow functions bind the balue of "this" of the function to the respective class. */
     {
         // this.state.qty++;
-        console.log(this.state);
+
         // set state form 1
         /* this.setState(
             {
                 qty:this.state.qty+1
             }
-        ) */
+        ) *//* in the form 1 as well we can pass the callback. */
 
         // set state form 2. it is used when the previous state is required.
-        this.setState((previousState)=>
+        this.setState((previousState) =>
         {
             return {
-                qty:previousState.qty+1
+                qty: previousState.qty + 1
             }
-        })
+        }, ()=>
+        {
+            console.log('state is changed', this.state);
+        });
+    }
+    decreaseQuantity = () =>
+    {
+        const {qty} =this.state;
+        if (qty > 1)
+            this.setState(
+                {
+                    qty: this.state.qty - 1
+                }
+            );
+        else
+            window.alert("You cant decrease the quantity below 1, if you want to delete the item, just click on the delete button!");
     }
     render()
     {
+        console.log('hello');
         const { price, title, qty } = this.state
         return (
             <div className="cart-item">
@@ -49,12 +65,12 @@ class CartItem extends React.Component
                         Rs. {price}
                     </div>
                     <div style={{ color: 'grey' }}>
-                        Quantity: {qty} {this.state.qty==1?'Unit':'Units'}
+                        Quantity: {qty} {this.state.qty == 1 ? 'Unit' : 'Units'}
                     </div>
                     <div className="cart-item-actions">
                         <div onClick={this.increaseQuantity}><i className="action-icons fas fa-trash-alt" ></i></div>
                         <div onClick={this.increaseQuantity}><i className="action-icons fas fa-plus-square" ></i></div>
-                        <div onClick={this.increaseQuantity}><i className="action-icons fas fa-minus-square"></i></div>
+                        <div onClick={this.decreaseQuantity}><i className="action-icons fas fa-minus-square"></i></div>
                     </div>
                 </div>
             </div>
