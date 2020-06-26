2c20d031531a46f0257da9e77ea1fb5df16793ac
Parikshit Singh <pk223933@gmail.com>
2020-06-26 08:00:08 +0530

    state shifted to App component and controlled navbar cart count

--- src/Cart.js
+++ src/Cart.js
@@ -2,96 +2,25 @@ import React from 'react';
 import CartItem from './CartItem';
 
 
-class Cart extends React.Component
+const Cart = (props) =>
 {
-    constructor()
-    {
-        super();
-        this.state = {
-            products: [
-                {
-                    price: 99,
-                    title: 'Watch',
-                    qty: 1,
-                    img: 0,
-                    id: 1
-                },
-                {
-                    price: 999,
-                    title: 'Phone',
-                    qty: 1,
-                    img: 0,
-                    id: 2
-                },
-                {
-                    price: 449,
-                    title: 'Television',
-                    qty: 1,
-                    img: 0,
-                    id: 3
-                },
-            ]
-        }
-    }
-    handleIncreaseQuantity = (product) =>
-    {
-        const { products } = this.state;
-        const index_of_required_product = products.indexOf(product);
-
-        products[index_of_required_product].qty += 1;
-
-        this.setState({
-            products
-        });
-    }
-    handleonDecreaseQuantity = (product) =>
-    {
-        const { products } = this.state;
-        const index_of_required_product = products.indexOf(product);
-
-        if (products[index_of_required_product].qty === 1)
-        {
-            window.alert("You can't reduce the quantity below 1, If you want to delete the product, just click on the delete button!")
-            return;
-        }
-
-        products[index_of_required_product].qty -= 1;
-
-        this.setState({
-            products
-        })
-    }
-    handleDeleteProduct=(product)=>//We can also use filter to filter the array where the id is not eqaul to the target id (we would have passed the target id in the props and cartitem, would have returned it to us when we call the onDelete function.)
-    {
-        const {products}=this.state;
-        const index_of_required_product=products.indexOf(product);
-
-        products.splice(index_of_required_product, 1);
-
-        this.setState({
-            products
-        });
-    }
-    render()
-    {
-        const { products } = this.state;
-        return (
-            <div className="cart">
-                {products.map((product) =>
-                {
-                    return (
-                        <CartItem
-                            product={product}
-                            key={product.id}
-                            onIncreaseQuantity={this.handleIncreaseQuantity}
-                            onDecreaseQuantity={this.handleonDecreaseQuantity}
-                            onDelete={this.handleDeleteProduct}
-                        />
-                    );
-                })}
-            </div>
-        );
-    }
+    const { products } = props;
+    return (
+        <div className="cart">
+            {products.map((product) =>
+            {
+                return (
+                    <CartItem
+                        product={product}
+                        key={product.id}
+                        onIncreaseQuantity={props.onIncreaseQuantity}
+                        onDecreaseQuantity={props.onDecreaseQuantity}
+                        onDelete={props.onDelete}
+                    />
+                );
+            })}
+        </div>
+    );
 };
 
 export default Cart;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const Navbar=(props) =>
+const Navbar = (props) =>
 {
-    console.log(props)
+    console.log(props.count)
     return (
         <div className='navbar' style={{ backgroundColor: 'black' }}>
             <div className="cart-icon-container">
                 <button>
                     <i className="fas fa-shopping-cart" style={{ color: 'white' }}></i>
-                    <span style={{ color: 'white' }}>3</span>
+                    {props.count>0?(<span style={{ color: 'white' }}>{props.count}</span>):''}
                 </button>
-
             </div>
         </div>
     )
