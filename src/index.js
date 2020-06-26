0b8f9b4358f669465afe353f1be0e6cf2b08dd78
Parikshit Singh <pk223933@gmail.com>
2020-06-26 14:31:29 +0530

    set up firebase

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
+import 'firebase/firestore'
 
-ReactDOM.render(
-  
-    <App />
-  ,
-  document.getElementById('root')
-);
+var firebaseConfig = {
+    apiKey: process.env.REACTIVE_CART_API_KEY,///////////REACTIVE_CART_API_KEY
+    authDomain: "reactive-cart.firebaseapp.com",
+    databaseURL: "https://reactive-cart.firebaseio.com",
+    projectId: "reactive-cart",
+    storageBucket: "reactive-cart.appspot.com",
+    messagingSenderId: process.env.REACTIVE_CART_MESSAGING_SENDER_ID,/////////////REACTIVE_CART_MESSAGING_SENDER_ID
+    appId: process.env.REACTIVE_CART_APP_ID//////////REACTIVE_CART_APP_ID
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<App />,document.getElementById('root'));
