77fb51f4d9a2c027d5789495e8ece2c52bca3866
Parikshit Singh <pk223933@gmail.com>
2020-07-01 13:17:29 +0530

    Updates

---
+++ precache-manifest.c7a7a0730f760dd76604433de0d74a91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afec585931fb39d778cfcf1cca1569b4",
+    "url": "/Reactive-Cart/index.html"
+  },
+  {
+    "revision": "c7a36678302659cd76fc",
+    "url": "/Reactive-Cart/static/css/main.0c4cfffa.chunk.css"
+  },
+  {
+    "revision": "a63f0265c5d2d0c4b589",
+    "url": "/Reactive-Cart/static/js/2.cc00d891.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/Reactive-Cart/static/js/2.cc00d891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a36678302659cd76fc",
+    "url": "/Reactive-Cart/static/js/main.55ca31b4.chunk.js"
+  },
+  {
+    "revision": "ec97b48a3ad8d1213e47",
+    "url": "/Reactive-Cart/static/js/runtime-main.a524c372.js"
+  }
+]);
